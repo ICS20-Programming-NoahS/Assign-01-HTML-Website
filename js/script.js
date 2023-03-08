6c95ad01644d6cf9a66fdfb0813f261caa844bb6
NoahSmith04 <124379601+NoahSmith04@users.noreply.github.com>
2023-03-08 04:59:44 +0000

    finished

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Noah Smith All rights reserved
+//
+// Created by: Noah Smith
+// Created on: Mar 2023
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "madden fun fact"
+  
+  document.getElementById("madden-nfl").innerHTML = "<i>The game franchise has appeared on 33 platforms! How cool is that?<i>"
+}
---
+++ js/script3.js
@@ -0,0 +1,5 @@
+// Copyright (c) 2023 Noah Smith All rights reserved
+//
+// Created by: Noah Smith
+// Created on: Mar 2023
+// This file contains the JS functions for pro_madden.html
